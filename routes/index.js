229030b18233f05d72d13337beb01b2542d4d43d
aaron97ovando <aaronovandoq19@gmail.com>
2021-09-23 03:04:32 -0500

    Proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Ovando Rodriguez Joaquin Aaron', appName:'WebApp' });
+});
+
+/* Agregaremos una ruta */
+router.get('/greeting', function(req, res, next){
+  res.send('Hola Como esta la Clase De Fullstack Web')
+})
+  
+module.exports = router;
