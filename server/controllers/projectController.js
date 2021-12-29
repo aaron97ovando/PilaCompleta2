725e87d5854181fdcee0d1f23dc72d617761472b
aaron97ovando <aaronovandoq19@gmail.com>
2021-12-28 23:54:36 -0600

    Procesando el Formulario

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -4,13 +4,29 @@ const index = (req, res) => {
   res.send('Respondiendo a "/Projects/index"');
 };
 
-// "/projects/add"
+//  GET "/projects/add"
+// Enviar el formulario para crear nuevas ideas
+// de Proyectos
 const add = (req, res) => {
   res.send('Respondiendo a "/projects/addView"');
+};
+
+// POST "/projects/add"
+// Procesa la informacion del formulario
+// req, res es el controlador de la funcion flecha
+const addPost = (req, res) => {
+  // Rescatando la informacion del formulario
+  // Apartados del formulario
+  const { name, description } = req.body;
+  res.json({
+    name,
+    description,
+  });
 };
 
 // Pendiente por programar
 export default {
   add,
+  addPost,
   index,
 };
--- server/routes/project.js
+++ server/routes/project.js
@@ -1,17 +1,20 @@
 // Importar Router
 import { Router } from 'express';
 
-// Todo: Importar el controlador de proyectos
+// Importando el controlador de proyectos
 import projectController from '@server/controllers/projectController';
 
-// Creando Instancia de Router
+// Creando Instancia de un Router
 const router = new Router();
 
 // "/projects" "/projects/index"
 router.get(['/', '/index'], projectController.index);
-
 // "/projects/add"
 // sirve el formulario para agregar proyectos
 router.get('/add', projectController.add);
+
+// POST "projects/add"
+// procesa el formulario
+router.post('/add', projectController.addPost);
 
 export default router;
